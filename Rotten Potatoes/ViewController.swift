3367a159f66c234faeb510edff2ad43e6b75514a
cTangonan123 <ctangonan@csumb.edu>
2025-08-03 12:22:40 -0700

    feat: base commit, will need to add README.md section on setting api keys

--- Rotten Potatoes/ViewController.swift
+++ Rotten Potatoes/ViewController.swift
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    if let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String {
+        print("Your API Key: \(apiKey)")
+    } else {
+        print("API Key not found!")
+    }
+    if let apiReadAccessToken = Bundle.main.object(forInfoDictionaryKey: "API_READ_ACCESS_TOKEN") as? String {
+      print("Your API Read Access Token: \(apiReadAccessToken)")
+    } else {
+      print("API Read Access Token not found!")
+    }
   }
 
 
